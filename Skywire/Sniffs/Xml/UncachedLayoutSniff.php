0bf92a2aa9f51ddafff7dc9f1efddb6d9391a52f
Neil <neil@skywire.co.uk>
2020-05-13 10:30:58 +0100

    Fix XML sniff line number reporting

--- Skywire/Sniffs/Xml/UncachedLayoutSniff.php
+++ Skywire/Sniffs/Xml/UncachedLayoutSniff.php
@@ -22,51 +22,16 @@ class UncachedLayoutSniff implements Sniff
     public function process(File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
-        foreach ($tokens as $position => $token) {
-            $noCache = strpos($token['content'], 'cacheable="false"') !== false;
-            if($noCache) {
-                $phpcsFile->addWarning('Using cacheable="false" in a layout renders the entire page uncacheable and should be avoided', $stackPtr, 'Skywire.Xml.UncachedLayout', [], 10);
-            }
+        $token = $tokens[$stackPtr];
+        $noCache = strpos($token['content'], 'cacheable="false"') !== false;
+        if ($noCache) {
+            $phpcsFile->addWarning(
+                'Using cacheable="false" in a layout renders the entire page uncacheable and should be avoided',
+                $stackPtr,
+                'Skywire.Xml.UncachedLayout',
+                [],
+                10
+            );
         }
     }
-
-//    protected $supportedTokenizers = ['XML'];
-//
-//    /**
-//     * Warning violation code.
-//     *
-//     * @var string
-//     */
-//    protected $warningCodeFoundHelper = 'UncachedLayoutFound';
-//
-//    /**
-//     * String representation of warning.
-//     *
-//     * @var string
-//     */
-//    protected $warningMessageFoundHelper = 'cacheable="false" renders the entire page uncacheable and should be avoided';
-//
-//    /**
-//     * @inheritdoc
-//     */
-//    public function register()
-//    {
-//        return [T_STRING];
-//    }
-//
-//    /**
-//     * @inheritdoc
-//     */
-//    public function process(File $phpcsFile, $stackPtr)
-//    {
-//        $tokens = $phpcsFile->getTokens();
-//        if ($tokens[$stackPtr]['content'] === '$this') {
-//            $position = $phpcsFile->findNext(T_STRING, $stackPtr, null, false, 'helper', true);
-//            if ($position !== false) {
-//                $phpcsFile->addWarning($this->warningMessageFoundHelper, $position, $this->warningCodeFoundHelper);
-//            } else {
-//                $phpcsFile->addWarning($this->warningMessageFoundThis, $stackPtr, $this->warningCodeFoundThis);
-//            }
-//        }
-//    }
 }
